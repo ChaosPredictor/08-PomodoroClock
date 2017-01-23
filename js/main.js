c9ac7b71607bac1f61ff67de25edc9f6c88ac10e
chaospredictor <kuzi81@gmail.com>
2017-01-24 01:34:23 +0200

    working timer that show in console minutes and second left

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-
 	
+	var timeout = 0;
+
 	document.getElementById("refresh").addEventListener("click", function(){
 		console.log("start run");
-		var timeoutID = window.setTimeout(clockEnd, [4000]);
+		//timeout = window.setTimeout(clockEnd, 4000);
+		a = new timer(clockEnd(),550000);
+
+		setInterval(function() {
+			console.log('Time left: ' + a.getTimeLeftMinutes(timeout)+ 'm : ' + a.getTimeLeftSeconds(timeout)+'s');
+		}, 1000);
 	});
 
 });
@@ -12,3 +18,56 @@ function clockEnd() {
 	$("body").css("background-color", "red");
 }
 
+
+function getTimeLeft(timeout) {
+	return Math.ceil((timeout._idleStart + timeout._idleTimeout - Date.now()) / 1000);
+}
+
+function timer(callback, delay) {
+	var id, started, remaining = delay, running
+
+	this.start = function() {
+		running = true
+		started = new Date()
+		id = setTimeout(callback, remaining)
+	}
+
+	this.pause = function() {
+		running = false
+		clearTimeout(id)
+		remaining -= new Date() - started
+	}
+
+	this.getTimeLeft = function() {
+		if (running) {
+			this.pause()
+			this.start()
+		}
+		
+		return remaining;
+	}
+
+	this.getTimeLeftSeconds = function() {
+		if (running) {
+			this.pause()
+			this.start()
+		}
+		
+		return Math.round(remaining/1000)%60;
+	}
+
+	this.getTimeLeftMinutes = function() {
+		if (running) {
+			this.pause()
+			this.start()
+		}
+		
+		return Math.floor(Math.round(remaining/1000)/60);
+	}
+
+	this.getStateRunning = function() {
+		return running
+	}
+
+	this.start();
+}
